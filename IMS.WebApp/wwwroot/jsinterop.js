caefcceaf3f5e01cf8a38898c0c671799cce224c
Hexin <hexin.email@qq.com>
2025-02-13 14:06:12 +0800

    使用Javascript互操作阻止表单Enter键自动提交

---
+++ IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,8 @@
+﻿function preventFormSubmission(formId) {
+    document.querySelector(`#${formId}`).addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            return false;
+        }
+    })
+}
