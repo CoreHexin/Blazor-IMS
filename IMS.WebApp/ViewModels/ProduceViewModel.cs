13c90aacd620aba3ce65cfdfde203f1b8547294b
Hexin <hexin.email@qq.com>
2025-02-13 13:38:10 +0800

    实现生产产品视图

---
+++ IMS.WebApp/ViewModels/ProduceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IMS.CoreBusiness;
+using IMS.WebApp.ViewModelValidations;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class ProduceViewModel
+    {
+        [Required]
+        public string ProductionNumber { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "该产品不存在")]
+        public int ProductId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        [Produce_EnsureEnoughInventoryQuantity]
+        public int QuantityToProduce { get; set; }
+
+        public Product? Product { get; set; }
+    }
+}
