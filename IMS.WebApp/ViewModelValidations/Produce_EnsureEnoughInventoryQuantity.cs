111e226ad5f1387fabdfa83d15536a0ce4e1e296
Hexin <hexin.email@qq.com>
2025-02-13 13:38:48 +0800

    添加生产数量验证

---
+++ IMS.WebApp/ViewModelValidations/Produce_EnsureEnoughInventoryQuantity.cs
@@ -0,0 +1,32 @@
+﻿using IMS.WebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModelValidations
+{
+    public class Produce_EnsureEnoughInventoryQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var produceViewModel = validationContext.ObjectInstance as ProduceViewModel;
+
+            if (produceViewModel is null || produceViewModel.Product is null)
+            {
+                return ValidationResult.Success;
+            }
+
+            foreach (var pi in produceViewModel.Product.ProductInventories)
+            {
+                if (pi.Inventory is not null 
+                    && pi.Inventory.Quantity < (produceViewModel.QuantityToProduce * pi.InventoryQuantity)) 
+                {
+                    return new ValidationResult(
+                        $"原材料 {pi.Inventory.Name} 数量不足", 
+                        new List<string>() { validationContext.MemberName }
+                    );
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
