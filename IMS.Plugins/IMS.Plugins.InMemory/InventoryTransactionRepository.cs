388254ab3b275e0d91e3921aaaa8e81de2c5bb3b
Hexin <hexin.email@qq.com>
2025-02-15 21:05:35 +0800

    新增库存变动报告用例

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
@@ -5,39 +5,107 @@ namespace IMS.Plugins.InMemory
 {
     public class InventoryTransactionRepository : IInventoryTransactionRepository
     {
+        private readonly IInventoryRepository _inventoryRepository;
         private List<InventoryTransaction> _inventoryTransaction = new List<InventoryTransaction>();
 
-        public async Task ProduceAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, decimal price)
+        public InventoryTransactionRepository(IInventoryRepository inventoryRepository)
         {
-            _inventoryTransaction.Add(new InventoryTransaction
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<InventoryTransaction>> GetTransactionsAsync(
+            string inventoryName,
+            DateTime? dateFrom,
+            DateTime? dateTo,
+            InventoryTransactionType? transactionType
+        ) 
+        {
+            var inventories = await _inventoryRepository.GetByNameAsync(inventoryName);
+
+            var query = inventories.Join(
+                _inventoryTransaction,
+                inventory => inventory.Id,
+                inventoryTransaction => inventoryTransaction.InventoryId,
+                (inventory, inventoryTransaction) => new InventoryTransaction
+                {
+                    OrderNumber = inventoryTransaction.OrderNumber,
+                    ProductionNumber = inventoryTransaction.ProductionNumber,
+                    InventoryId = inventoryTransaction.InventoryId,
+                    QuantityBefore = inventoryTransaction.QuantityBefore,
+                    QuantityAfter = inventoryTransaction.QuantityAfter,
+                    UnitPrice = inventoryTransaction.UnitPrice,
+                    TransactionDate = inventoryTransaction.TransactionDate,
+                    DoneBy = inventoryTransaction.DoneBy,
+                    ActivityType = inventoryTransaction.ActivityType,
+                    Inventory = inventory
+                }
+            );
+
+            if (transactionType is not null)
             {
-                ProductionNumber = productionNumber,
-                InventoryId = inventory.Id,
-                QuantityBefore = inventory.Quantity,
-                QuantityAfter = inventory.Quantity - quantityToConsume,
-                UnitPrice = price,
-                TransactionDate = DateTime.Now,
-                DoneBy = doneBy,
-                ActivityType = InventoryTransactionType.ProduceProduct,
-                Inventory = inventory
-            });
+                query = query.Where(x => x.ActivityType == transactionType);
+            }
+
+            if (dateFrom is not null)
+            {
+                query = query.Where(x => x.TransactionDate >= dateFrom);
+            }
+
+            if (dateTo is not null)
+            {
+                query = query.Where(x => x.TransactionDate <= dateTo);
+            }
+
+            return query;
+        }
+
+        public async Task ProduceAsync(
+            string productionNumber,
+            Inventory inventory,
+            int quantityToConsume,
+            string doneBy,
+            decimal price
+        )
+        {
+            _inventoryTransaction.Add(
+                new InventoryTransaction
+                {
+                    ProductionNumber = productionNumber,
+                    InventoryId = inventory.Id,
+                    QuantityBefore = inventory.Quantity,
+                    QuantityAfter = inventory.Quantity - quantityToConsume,
+                    UnitPrice = price,
+                    TransactionDate = DateTime.Now,
+                    DoneBy = doneBy,
+                    ActivityType = InventoryTransactionType.ProduceProduct,
+                    Inventory = inventory,
+                }
+            );
             await Task.CompletedTask;
         }
 
-        public async Task PurchaseAsync(string orderNumber, Inventory inventory, int quantity, string doneBy, decimal price)
+        public async Task PurchaseAsync(
+            string orderNumber,
+            Inventory inventory,
+            int quantity,
+            string doneBy,
+            decimal price
+        )
         {
-            _inventoryTransaction.Add(new InventoryTransaction
-            {
-                OrderNumber = orderNumber,
-                InventoryId = inventory.Id,
-                QuantityBefore = inventory.Quantity,
-                QuantityAfter = inventory.Quantity + quantity,
-                UnitPrice = price,
-                TransactionDate = DateTime.Now,
-                DoneBy = doneBy,
-                ActivityType = InventoryTransactionType.PurchaseInventory,
-                Inventory = inventory
-            });
+            _inventoryTransaction.Add(
+                new InventoryTransaction
+                {
+                    OrderNumber = orderNumber,
+                    InventoryId = inventory.Id,
+                    QuantityBefore = inventory.Quantity,
+                    QuantityAfter = inventory.Quantity + quantity,
+                    UnitPrice = price,
+                    TransactionDate = DateTime.Now,
+                    DoneBy = doneBy,
+                    ActivityType = InventoryTransactionType.PurchaseInventory,
+                    Inventory = inventory,
+                }
+            );
             await Task.CompletedTask;
         }
     }
--- IMS.UseCases/PluginInterfaces/IInventoryTransactionRepository.cs
+++ IMS.UseCases/PluginInterfaces/IInventoryTransactionRepository.cs
@@ -4,7 +4,27 @@ namespace IMS.UseCases.PluginInterfaces
 {
     public interface IInventoryTransactionRepository
     {
-        Task PurchaseAsync(string orderNumber, Inventory inventory, int quantity, string doneBy, decimal price);
-        Task ProduceAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, decimal price);
+        Task PurchaseAsync(
+            string orderNumber,
+            Inventory inventory,
+            int quantity,
+            string doneBy,
+            decimal price
+        );
+
+        Task ProduceAsync(
+            string productionNumber,
+            Inventory inventory,
+            int quantityToConsume,
+            string doneBy,
+            decimal price
+        );
+
+        Task<IEnumerable<InventoryTransaction>> GetTransactionsAsync(
+            string inventoryName,
+            DateTime? dateFrom,
+            DateTime? dateTo,
+            InventoryTransactionType? transactionType
+        );
     }
 }
